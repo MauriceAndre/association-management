e39fb8bbb89ccc9bd962dd295634729eaa396a67
Maurice Schmid <maurice.schmid@live.de>
2019-12-06 19:43:50 -0800

    Docker setup

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello Canada!');
 });
 
 app.listen(3000, () => console.log('Server is listening on port 3000...'));
